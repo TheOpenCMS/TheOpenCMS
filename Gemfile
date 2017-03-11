f6c5a4c0819af5248a2d0a978aca2dfa75e5406d
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-03-11 16:54:04 +0300

    Add JQuery/JQuery UI Rails

--- Gemfile
+++ Gemfile
@@ -5,6 +5,13 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'jquery-rails',
+  github: 'rails/jquery-rails',
+  branch: '89d60928'
+
+gem 'jquery-ui-rails',
+  github: 'jquery-ui-rails/jquery-ui-rails',
+  branch: '0b22d466'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.0.beta1'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's 
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
