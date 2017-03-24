93183314bc85f5afa12fe3fd45ea5528c108f0c4
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-03-24 10:44:11 +0300

    User Room. Improvements in controllers and locales

--- app/controllers/devise_controllers/sessions_controller.rb
+++ app/controllers/devise_controllers/sessions_controller.rb
@@ -8,26 +8,26 @@ class DeviseControllers::SessionsController < Devise::SessionsController
     user_for_login = User.where(email: _email).first
 
     if _email.blank? || !_email.match(/@/) || user_for_login.blank?
-      return redirect_to :back, alert: 'Запрос не может быть выполнен'
+      return redirect_to :back, alert: _t(:cant_perform_request)
     end
 
-    if OnetimeLoginLink.where("email = ? AND created_at > ?", _email, 2.minutes.ago).first
-      return redirect_to :back, alert: 'Запрос на одноразовый вход уже получен. Выполните вход или немного подождите'
+    if ::OnetimeLoginLink.where("email = ? AND created_at > ?", _email, 2.minutes.ago).first
+      return redirect_to :back, alert: _t(:request_already_sent)
     end
 
-    log_req = OnetimeLoginLink.create(email: _email)
-    DeviseMailer.onetime_login_request(log_req.id, callback_path).deliver_now
-    redirect_to :back, alert: 'Запрос на одноразовый вход сформирован. Перейдите в свой почтовый ящик'
+    log_req = ::OnetimeLoginLink.create(email: _email)
+    ::DeviseMailer.onetime_login_request(log_req.id, callback_path).deliver_now
+    redirect_to :back, alert: _t(:check_inbox)
   end
 
   def activate_onetime_login_link
     uid = params[:uid]
     callback_path = params[:callback_path]
 
-    log_req = OnetimeLoginLink.where(uid: uid).first
+    log_req = ::OnetimeLoginLink.where(uid: uid).first
 
     unless log_req
-      return redirect_to root_path, alert: 'Запрос на одноразовый вход не действителен'
+      return redirect_to root_path, alert: _t(:wrong_request)
     end
 
     log_req.update_attributes(
@@ -35,11 +35,11 @@ class DeviseControllers::SessionsController < Devise::SessionsController
       created_at: 1.year.ago
     )
 
-    user = User.where(email: log_req.email).first
+    user = ::User.where(email: log_req.email).first
     sign_in user
 
     next_page = callback_path.present? ? callback_path : cabinet_path
-    redirect_to next_page, notice: 'Выполнен вход на сайт'
+    redirect_to next_page, notice: _t(:logined_in)
   end
 
   # POST /resource/sign_in
@@ -55,6 +55,10 @@ class DeviseControllers::SessionsController < Devise::SessionsController
   end
 
   private
+
+  def _t(name)
+    t(name, scope: 'user_room.controllers.devise.sessions')
+  end
 
   def after_sign_out_path_for(resource)
     cookies[:previous_location] || super
