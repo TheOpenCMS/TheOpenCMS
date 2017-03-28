b639ada7e9adb3cf0f3a9dc0a117a36b130fbd0d
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-03-28 21:51:12 +0300

    User Room. Add validations

--- app/models/concerns/user_room/user.rb
+++ app/models/concerns/user_room/user.rb
@@ -26,39 +26,52 @@ module UserRoom
       include ::UserRoom::SocialNetworks::Login
       include ::UserRoom::SocialNetworks::Validations
 
-      before_validation :create_default_login, on: :create
-      before_validation :prepare_login
-      before_validation :set_login_if_this_login_already_exists_or_blank, on: :create
+      ##########################################
+      ### Validations
+      ##########################################
 
-      # Validations
-      validates :login, presence: true, uniqueness: true
-      validate  :login_chars_required
-
+      validates :login, presence: true, uniqueness: true, on: :update, if: :login_changed?
+      validate  :login_validations, on: :update, if: :login_changed?
       validates :password, length: { minimum: 6 }, if: ->{ password.present? }
+
+      ##########################################
+      ### Callbacks
+      ##########################################
+
+      after_create :create_default_login
+
+      ##########################################
+      ### Relations
+      ##########################################
 
       has_many :orders
 
-      def login_chars_required
-        errors.add(:login, "has to have a one of more letters") unless (self.login =~ /[a-zA-Z]/mix)
+      ##########################################
+      ### Other methods
+      ##########################################
+
+      def to_param
+        self.login
       end
 
       private
 
       def create_default_login
-        _login = "user-#{ SecureRandom.hex[0..4] }"
-        self.login = self.login.blank? ? _login : self.login
+        self.login = "id#{self.id}"
       end
 
-      def prepare_login
-        self.login = self.login.to_s.to_slug_param
-      end
+      def login_validations
+        unless (self.login =~ /[a-zA-Z]/mix)
+          errors.add(:login, "has to have a one of more letters")
+        end
 
-      def set_login_if_this_login_already_exists_or_blank
-        if self.class.where(login: self.login).first || self.login.blank?
-          self.login = "user-#{ SecureRandom.hex[0..6] }"
+        if self.login =~ /^id/mix
+          if self.login.downcase != "id#{self.id}"
+            errors.add(:login, "This Login could not be used")
+          end
         end
       end
 
-    end # included
+    end # included do
   end # User
 end # UserRoom
