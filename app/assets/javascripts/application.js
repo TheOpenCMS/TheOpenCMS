59b06acef2d15ed4a6ccb847dd227eb01f0737df
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-03-28 11:25:19 +0300

    AppBase. Models and code cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery3
 //= require rails-ujs
 //= require turbolinks
-
 //= require cable
-//= require JODY
-//= require search
-//= require app_initializer
+
--- app/models/note.rb
+++
@@ -1,8 +0,0 @@
-class Note < ApplicationRecord
-  belongs_to :user
-
-  validates :title,   presence: true
-  validates :content, presence: true
-
-  scope :random, ->{ order('RANDOM()') }
-end
