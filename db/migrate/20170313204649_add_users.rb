efdf1c2b08be480aac202fdb6197c24cd2b98184
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-03-29 10:57:16 +0300

    Improvments

--- db/migrate/20170313204649_add_users.rb
+++ db/migrate/20170313204649_add_users.rb
@@ -1,5 +1,7 @@
 class AddUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users
+    create_table :users do |t|
+      t.timestamps null: false
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20170313205017) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
