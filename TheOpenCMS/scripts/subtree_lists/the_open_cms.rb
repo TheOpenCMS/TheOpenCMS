40f2e74a4c4cf863d81734d98beb1f1dcd52802e
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-04-08 07:44:13 +0300

    TheOpenCMS subtrees list

---
+++ TheOpenCMS/scripts/subtree_lists/the_open_cms.rb
@@ -0,0 +1,37 @@
+SUBTREES = %w[
+  TheOpenCMS/voiceless
+  git@github.com:TheOpenCMS/voiceless.git
+  master
+
+  TheOpenCMS/user_room
+  git@github.com:TheOpenCMS/user_room.git
+  master
+
+  TheOpenCMS/pagination
+  git@github.com:TheOpenCMS/pagination.git
+  master
+
+  TheOpenCMS/log_js
+  git@github.com:TheOpenCMS/log_js.git
+  master
+
+  TheOpenCMS/notifications
+  git@github.com:TheOpenCMS/notifications.git
+  master
+
+  TheOpenCMS/simple_sort
+  git@github.com:TheOpenCMS/simple_sort.git
+  master
+
+  TheOpenCMS/image_tools
+  git@github.com:TheOpenCMS/image_tools.git
+  master
+
+  TheOpenCMS/crop_tool
+  git@github.com:TheOpenCMS/crop_tool.git
+  master
+
+  TheOpenCMS/app_base
+  git@github.com:TheOpenCMS/app_base.git
+  master
+]
