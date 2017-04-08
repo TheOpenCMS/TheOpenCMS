f648978779ee96529c78cc9d7af25b28f46c5553
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-04-08 07:33:27 +0300

    Script updated

---
+++ TheOpenCMS/scripts/subtree.rb
@@ -0,0 +1,52 @@
+# ruby TheOpenCMS/scripts/subtree protozaur
+
+action = ARGV.first.strip.downcase
+list   = ARGV.last.strip.downcase + '.rb'
+
+require_relative "subtree_lists/#{list}"
+
+subtrees_dir = 'TheOpenCMS/Engines'
+subtrees = SUBTREES.each_slice(3)
+
+if action == 'add'
+  subtrees.each do |item|
+    local_path  = item[0]
+    remote_repo = item[1]
+    branch      = item[2]
+
+    puts `
+      git subtree add \
+        --prefix #{subtrees_dir}/#{local_path} \
+        #{remote_repo} \
+        #{branch} --squash
+    `
+  end
+elsif action == 'push'
+  subtrees.each do |item|
+    local_path  = item[0]
+    remote_repo = item[1]
+    branch      = item[2]
+
+    puts `
+      git subtree push \
+        --prefix #{subtrees_dir}/#{local_path} \
+        #{remote_repo} \
+        #{branch}
+    `
+  end
+elsif action == 'pull'
+  subtrees.each do |item|
+    local_path  = item[0]
+    remote_repo = item[1]
+    branch      = item[2]
+
+    puts `
+      git subtree pull \
+        --prefix #{subtrees_dir}/#{local_path} \
+        #{remote_repo} \
+        #{branch}
+    `
+  end
+else
+  puts 'Undefined action. Acceptable values: [ add | push | pull ]'
+end
---
+++ TheOpenCMS/scripts/subtree_lists/protozaur.rb
@@ -0,0 +1,5 @@
+SUBTREES = %w[
+  Protozaur/protozaur
+  git@github.com:the-teacher/protozaur.git
+  master
+]
