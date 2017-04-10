316e0080af22fc08ce0b7bd00991c2eae372f550
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-04-10 10:34:08 +0300

    DeployTool. Update Yarn Script

--- kit/rails/yarn.rb
+++ kit/rails/yarn.rb
@@ -1,13 +1,5 @@
+# https://yarnpkg.com/lang/en/docs/install/
 class DeployKit
-  def yarn_install
-    # sudo_remote_exec [
-    #   "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -",
-    #   'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list',
-    #   'apt-get update',
-    #   'apt-get install yarn'
-    # ]
-  end
-
   def yarn_bundle
     remote_exec [
       "cd #{current_path}",
