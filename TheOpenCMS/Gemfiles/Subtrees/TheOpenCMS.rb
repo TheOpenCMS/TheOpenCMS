e30a8e282126f7873ac46a1107278a564a39ea3e
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-04-29 11:58:51 +0300

    Simple authorization solution

--- TheOpenCMS/Gemfiles/Subtrees/TheOpenCMS.rb
+++ TheOpenCMS/Gemfiles/Subtrees/TheOpenCMS.rb
@@ -6,6 +6,7 @@ gem 'image_tools',       path: '../../Engines/TheOpenCMS/image_tools'
 gem 'crop_tool',         path: '../../Engines/TheOpenCMS/crop_tool'
 gem 'log_js',            path: '../../Engines/TheOpenCMS/log_js'
 
+gem 'authorize_it',      path: '../../Engines/TheOpenCMS/authorize_it'
 gem 'user_room',         path: '../../Engines/TheOpenCMS/user_room'
 gem 'app_base',          path: '../../Engines/TheOpenCMS/app_base'
 gem 'app_theme',         path: '../../Engines/Addons/theopencms.org'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include ::AutorizeIt::Controller
+  include ::AuthorizeIt::Controller
   protect_from_forgery with: :exception
 
   before_action -> { @user = current_user }
