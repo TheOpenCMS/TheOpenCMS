f2016495293d6c88519082f31c1df1d1d1523135
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-06-18 13:09:04 +0300

    AuthorizeIt ACL interface

---
+++ lib/authorize_it/acl_permits.rb
@@ -0,0 +1,56 @@
+module AuthorizeIt::ACLPermits
+  # class ArticleACL::Update < AuthorizeIt::ACLPermits::Base
+  #   def can_perform?
+  #     return true if @user.admin?
+  #
+  #     acl = {
+  #       user: %w[ index show ]
+  #       editor: %w[ index show edit update destroy]
+  #     }
+  #
+  #     role = @user.editor? ? :editor : :user
+  #     acl[role].include?(@action)
+  #   end
+  # end
+
+  # ArticleACL::Update.new(
+  #   user: current_user,
+  #   action: 'update',
+  # ).can_perform?
+
+  class Base
+    def initialize(options = {})
+      @options = options
+
+      @user = options[:user]
+      @scope = options[:scope]
+      @action = options[:action]
+      @resource = options[:resource]
+    end
+  end
+
+  private
+
+  #################################
+  # Controller Helper
+  #################################
+
+  def can_perform?(options = {})
+    user = options.fetch(:user, self.try(:current_user))
+    scope = options.fetch(:scope, self.try(:controller_name))
+    action = options.fetch(:action, self.try(:action_name))
+    resource = options.fetch(:resource, nil)
+
+    options = options.merge({
+      user: user,
+      scope: scope,
+      action: action,
+      resource: resource
+    })
+
+    acl_klass_name = "#{ scope.singularize.to_s.classify }ACL::#{ action.to_s.classify }"
+    acl_klass = acl_klass_name.constantize
+
+    acl_klass.new(options).can_perform?
+  end
+end
--- lib/authorize_it/permitted_params.rb
+++ lib/authorize_it/permitted_params.rb
@@ -9,6 +9,10 @@ module AuthorizeIt::PermittedParams
 
   private
 
+  #################################
+  # Controller Helper
+  #################################
+
   def permitted_params(options = {})
     controller_name = options.fetch(:controller, self.controller_name)
     action_name     = options.fetch(:action, self.action_name)
@@ -18,7 +22,7 @@ module AuthorizeIt::PermittedParams
       action_name: action_name
     })
 
-    permit_klass_name = "#{controller_name.singularize}_permits/#{action_name}".classify
+    permit_klass_name = "#{ controller_name.singularize }_permits/#{ action_name }".classify
     permit_klass = (permit_klass_name).constantize
 
     permit_klass.new(self, options).permitted_params
