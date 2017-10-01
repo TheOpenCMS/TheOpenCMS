f4a001a555b0cd0db3aa5a6b8833e9d4db6fb336
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-10-01 18:47:16 +0300

    rm simple_sort

--- Engines/tools/simple_sort/tests/dummy_app/app/models/post.rb
+++
@@ -1,3 +0,0 @@
-class Post < ApplicationRecord
-  include ::SimpleSort::Base
-end
--- Engines/tools/simple_sort/tests/dummy_app/spec/factories/posts.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :post do
-    sequence(:title) { |n| "Post #{n}" }
-    body { Forgery(:lorem_ipsum).words(5) }
-  end
-end
